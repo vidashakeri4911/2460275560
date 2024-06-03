b9cd3fc60325a27b00433c5b63f6e3ddbeec4546
vidashakeri4911 <167344679+vidashakeri4911@users.noreply.github.com>
2024-06-03 22:35:29 +0330

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+
+using namespace std;
+struct email{
+    char address[255];
+    bool isvalid()
+    {
+        int countAtsign=0,positionAt=-1,positionDot=-1;
+        for(int i=0;address[i]!='\0';i++){
+            if(address[i]=='@'){
+                countAtsign++;
+                if(positionAt==-1){
+                    positionAt=i;
+                }
+            }
+            if(address[i]=='.'){
+                positionDot=i;
+            }
+
+  }
+     if(countAtsign==1&&positionAt!=0&&positionAt<positionDot){
+        return true;
+     }
+     return false;
+    }
+};
+int main()
+{
+    email e;
+    cin>>e.address;
+    if(e.isvalid()){
+        cout<<"I valid";
+    }
+
+
+    return 2;
+}
+
