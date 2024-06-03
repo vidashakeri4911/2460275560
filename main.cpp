abe554e434f708f72d31ca27346cd8ef2146851b
vidashakeri4911 <167344679+vidashakeri4911@users.noreply.github.com>
2024-06-03 23:19:39 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,26 +1,16 @@
+
 #include <iostream>
-
-using namespace std;
-
 int main()
 {
-    float grade;
-    cout<<"enter grade:";
-    cin>>grade;
-    if(grade>=17){
-        cout<<"a level";
-    }
-    if(grade>=14&&grade<17){
-        cout<<"b level";
-    }
-    if(grade>=10&&grade<14){
-        cout<<"c level";
-    }
-    if(grade<10){
-        cout<<"d level";
-    }
-    if(grade<12){
-        cout<<"mashroot!";
-    }
-    return 0;
+   int i,j ;
+   for(i = 1 ; i<= 3 ; i++)
+   {
+       for(j=1;j<=i;j++)
+       {
+           printf("*");
+       }
+   printf("\n");
+   }
+
+   return 0;
 }
