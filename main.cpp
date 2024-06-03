5688145c09356d3fe3701327f460d131ac9923f6
vidashakeri4911 <167344679+vidashakeri4911@users.noreply.github.com>
2024-06-03 23:03:35 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,38 +1,9 @@
 #include <iostream>
-#include<cstring>
+
 using namespace std;
-bool check(char pass[255],char s='0',char e='9'){
-    for(int i=0;pass[i]!='\0';i++){
-        if(s>=pass[i]&&e<=pass[i]){
-            return true;
-        }
-        }
-        return false;
-    }
+
 int main()
 {
-    char pass[255];
-    cin>>pass;
-    if(strlen(pass)<6){
-        cout<<"Bod password";
-    }
-    int dificality=0;
-    if(check(pass)){
-        dificality++;
-    }
-    if(check(pass,'!','/')){
-        dificality++;
-    }
-    if(check(pass,'a','7')){
-        dificality++;
-    }
-    if(check(pass,'A','7')){
-        dificality++;
-    }
-    if(strlen(pass)>8){
-        dificality++;
-    }
-    cout<<"Dificality:"<<(dificality*20);
+    cout << "Hello world!" << endl;
     return 0;
 }
-
