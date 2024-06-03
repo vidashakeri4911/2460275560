d44ebfde06cfc2ec551858fecd0b5f237d81f3d3
vidashakeri4911 <167344679+vidashakeri4911@users.noreply.github.com>
2024-06-03 23:00:35 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,26 +1,38 @@
-
 #include <iostream>
-
+#include<cstring>
 using namespace std;
-struct mobile{
-    char number[11];
-    bool isvalid()
-    {
-        if(number[0]=='0'&&number[1]=='9'){
-            return 1;
+bool check(char pass[255],char s='0',char e='9'){
+    for(int i=0;pass[i]!='\0';i++){
+        if(s>=pass[i]&&e<=pass[i]){
+            return true;
         }
-        return 0;
+        }
+        return false;
     }
-};
-
 int main()
 {
- mobile mobile;
- cin>>mobile.number;
- if(mobile.isvalid()){
-    cout<<"valid";
- }else{
-     cout<<"valid";
- }
-return 1;
+    char pass[255];
+    cin>>pass;
+    if(strlen(pass)<6){
+        cout<<"Bod password";
+    }
+    int dificality=0;
+    if(check(pass)){
+        dificality++;
+    }
+    if(check(pass,'!','/')){
+        dificality++;
+    }
+    if(check(pass,'a','7')){
+        dificality++;
+    }
+    if(check(pass,'A','7')){
+        dificality++;
+    }
+    if(strlen(pass)>8){
+        dificality++;
+    }
+    cout<<"Dificality:"<<(dificality*20);
+    return 0;
 }
+
