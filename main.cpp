0524070e392f764ee8c91df7a06dfe0632882118
vidashakeri4911 <167344679+vidashakeri4911@users.noreply.github.com>
2024-06-03 23:24:26 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,25 +1,22 @@
 
 
+
 #include <iostream>
 using namespace std;
 int main()
 {
-  int number;
-  int baghi;
-  int sum = 0;
+    cout<<"       ***"<<endl;
+    cout<<"        ***"<<endl;
+    cout<<"         ***"<<endl;
+    cout<<"          ***"<<endl;
+    cout<<"           ***"<<endl;
+    cout<<"***************"<<endl;
+    cout<<"           ***"<<endl;
+    cout<<"          ***"<<endl;
+    cout<<"         ***"<<endl;
+    cout<<"        ***"<<endl;
+    cout<<"       ***"<<endl;
+    cout<<"      ***";
 
-  cout << "enter number : ";
-  cin >> number;
-
-  while(number != 0)
-  {
-
-      baghi = number % 10;
-      sum = sum * 10 + baghi;
-      number /= 10;
-
-  }
-      cout << "reverse value is "<< sum << endl;
-
-       return 0;
+    return 0;
 }
