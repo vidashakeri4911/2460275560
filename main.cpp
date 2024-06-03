a4496479279f68974ce333f58dd2fab6bb9d99af
vidashakeri4911 <167344679+vidashakeri4911@users.noreply.github.com>
2024-06-03 23:18:03 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,39 +1,26 @@
 #include <iostream>
-#include<cstring>
+
 using namespace std;
-struct validcard{
-    char prefix[4];
-    char bank[30];
-};
-  validcard vcs[3];
-  vcs[0].prefix="5041";
-  vcs[0].name="Resalat";
-  vcs[1].prefix="6037";
-  vcs[1].bank="meli";
-  vcs[2].prefix-"6104";
-  vcs[2].bank="Mellat";
-  struct card{
-      char number[16];
-      char bank[30];
-      bool isvolid()
-      {
-          for(int i=0;i<3;i++){
-            if(!strncmp(number,ves[i].prefix,4)){
-                strcmp(bank,vcs[i].name);
-                return true;
-            }
-          }
-          return false;
-      }
-  };
+
 int main()
 {
-    card card;
-    cin>>card.number;
-    if(card.isvalid()){
-        cout<<card.bank;
-    }else{
-        cout<<"Bad!";
+    float grade;
+    cout<<"enter grade:";
+    cin>>grade;
+    if(grade>=17){
+        cout<<"a level";
+    }
+    if(grade>=14&&grade<17){
+        cout<<"b level";
+    }
+    if(grade>=10&&grade<14){
+        cout<<"c level";
+    }
+    if(grade<10){
+        cout<<"d level";
+    }
+    if(grade<12){
+        cout<<"mashroot!";
     }
     return 0;
 }
