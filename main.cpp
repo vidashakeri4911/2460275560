2bfcced45837a3f8163738d62c3cb3608c70ee2a
vidashakeri4911 <167344679+vidashakeri4911@users.noreply.github.com>
2024-06-03 23:09:10 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,61 +1,39 @@
 #include <iostream>
-
+#include<cstring>
 using namespace std;
-int pow(int a,int b){
-    if(b==0){
-        return 1;
-    }
-    return a*pow(a,b-1);
-}
-int fibo(int n){
-    if(n==1||(n==2)){
-        return 1;
-    }
-    return fibo(n-2)+fibo(n-1);
-}
-int zarb(int a,int b){
-       if(b==0){
-        return 1;
-       }
-       return a+zarb(a,b-1);
-}
-  int faet(int n){
-      if(n==1){
-        return 6;
+struct validcard{
+    char prefix[4];
+    char bank[30];
+};
+  validcard vcs[3];
+  vcs[0].prefix="5041";
+  vcs[0].name="Resalat";
+  vcs[1].prefix="6037";
+  vcs[1].bank="meli";
+  vcs[2].prefix-"6104";
+  vcs[2].bank="Mellat";
+  struct card{
+      char number[16];
+      char bank[30];
+      bool isvolid()
+      {
+          for(int i=0;i<3;i++){
+            if(!strncmp(number,ves[i].prefix,4)){
+                strcmp(bank,vcs[i].name);
+                return true;
+            }
+          }
+          return false;
       }
-....  return n*fact(n-1);
-  }
-  struct calcultor{
-      int lv,rv;
-      char op;
-      void run()
-  {
-      cout<<"Enter oprtor:";
-        cin>>op;
-        switch(op){
-            case'^':
-             cin>>lv>>rv;
-             cout<<pow(lv,rv);
-             break;
-             case'*':
-             cin>>lv>>rv;
-             cout<<lv<<rv;
-             break;
-             case'!':
-             cin>>lv;
-             cout<<foet(lv);
-             break;
-             detalt:
-                 cout<<"Bad!";
-
-        }
-
   };
-
-
 int main()
 {
-    calculator c;
-    c.run();
+    card card;
+    cin>>card.number;
+    if(card.isvalid()){
+        cout<<card.bank;
+    }else{
+        cout<<"Bad!";
+    }
     return 0;
 }
