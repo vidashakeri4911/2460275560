85fde8fb7019dd24e2f11d6d8ab70c38144d3901
vidashakeri4911 <167344679+vidashakeri4911@users.noreply.github.com>
2024-06-04 13:04:45 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,22 +1,43 @@
 
-
-
 #include <iostream>
+#include<cstring>
 using namespace std;
+struct valiCard{
+    char prefix[4];
+    char bank[30];
+};
+valiCard vcs[3];
+void x(){
+  strcpy(vcs[0].prefix,"5041");
+  strcpy(vcs[0].bank,"Resalat");
+  strcpy(vcs[1].prefix,"6037");
+  strcpy(vcs[1].bank,"Meli");
+  strcpy(vcs[2].prefix,"6104");
+  strcpy(vcs[2].bank,"Mellat");
+}
+  struct card{
+      char number[16];
+      char bank[30];
+      bool isvalid()
+      {
+          for(int i=0;i<3;i++){
+            if(!strncmp(number,vcs[i].prefix,4)){
+                strcpy(bank,vcs[i].bank);
+                return true;
+            }
+          }
+          return false;
+      }
+  };
 int main()
 {
-    cout<<"       ***"<<endl;
-    cout<<"        ***"<<endl;
-    cout<<"         ***"<<endl;
-    cout<<"          ***"<<endl;
-    cout<<"           ***"<<endl;
-    cout<<"***************"<<endl;
-    cout<<"           ***"<<endl;
-    cout<<"          ***"<<endl;
-    cout<<"         ***"<<endl;
-    cout<<"        ***"<<endl;
-    cout<<"       ***"<<endl;
-    cout<<"      ***";
 
+    card card;
+    cin>>card.number;
+    if(card.isvalid()){
+        cout<<card.bank;
+    }else{
+        cout<<"Bad!";
+    }
     return 0;
 }
