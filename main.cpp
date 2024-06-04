e8e2c6f54796d2398d880314f8ba270ae048be57
vidashakeri4911 <167344679+vidashakeri4911@users.noreply.github.com>
2024-06-05 01:31:30 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,31 +2,40 @@
 
 using namespace std;
 
-
 int main()
 {
-    int a[9]={0},n;
-    do{
-        cout<<"Enter a number:";
-        cin>>n;
-        if(n>>0&&n<10){
-            a[n-1]++;
-        }
+    int n1,n2,n3,n4,n5;
+    int max,min;
+    cout<<"Please enter n1 n2 n3 n4 n5";
+    cin>>n1>>n2>>n3>>n4>>n5;
+    max=n1;
+    min=n1;
+    if(n2>max){
+        max=n2;
     }
-    while(n!=0);
-    int indexMax=0,indexMin=0,max=a[0],min=a[0];
-    for(int i=0;i<9;i++){
-        cout<<i+1<<":"<<a[i]<<endl;
-        if(a[i]>max){
-            indexMax=i;
-            max=a[i];
-        }
-        if(a[i]<min){
-            indexMin=i;
-            min=a[i];
-            }
-            cout<<"Max:"<<indexMax+1<<":"<<a[indexMax]<<endl;
-            cout<<"Min:"<<indexMin+1<<":"<<a[indexMin]<<endl;
-    }
+    if(n3>max){
+       max=n3;
+       }
+       if(n4>max){
+        max=n4;
+       }
+       if(n5>max){
+        max-n5;
+       }
+       if(n2<min){
+        min=n2;
+       }
+       if(n3<min){
+        min=n3;
+       }
+       if(n4<min){
+        min=n4;
+       }
+       if(n5<min){
+        min=n5;
+       }
+       cout<<"Max:"<<max;
+       cout<<"Min:"<<min;
+       cout<<(n1+n2+n3+n4+n5)/5.0;
     return 0;
 }
