a5601bd9548b809b319a015f02715eee04334502
vidashakeri4911 <167344679+vidashakeri4911@users.noreply.github.com>
2024-06-05 00:00:02 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,14 +2,31 @@
 
 using namespace std;
 
+
 int main()
 {
-    int hour;
-    cin>>hour;
-    if(hour>12){
-        cout<<"PM";
-    }else{
-        cout<<"AM";
+    int a[9]={0},n;
+    do{
+        cout<<"Enter a number:";
+        cin>>n;
+        if(n>>0&&n<10){
+            a[n-1]++;
+        }
+    }
+    while(n!=0);
+    int indexMax=0,indexMin=0,max=a[0],min=a[0];
+    for(int i=0;i<9;i++){
+        cout<<i+1<<":"<<a[i]<<endl;
+        if(a[i]>max){
+            indexMax=i;
+            max=a[i];
+        }
+        if(a[i]<min){
+            indexMin=i;
+            min=a[i];
+            }
+            cout<<"Max:"<<indexMax+1<<":"<<a[indexMax]<<endl;
+            cout<<"Min:"<<indexMin+1<<":"<<a[indexMin]<<endl;
     }
     return 0;
 }
