379ce1c2ec79599f648646dff8b72934a85824e0
vidashakeri4911 <167344679+vidashakeri4911@users.noreply.github.com>
2024-06-04 21:13:07 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,66 +1,12 @@
-
 #include <iostream>
-
 using namespace std;
-int pow(int a,int b){
-    if(b==0){
-        return 1;
-    }
-    return a*pow(a,b-1);
-}
-int fibo(int n){
-    if(n==1||n==2){
-        return 1;
-    }
-    return fibo(n-2)+fibo(n-1);
-}
-int zarb(int a,int b){
-       if(b==0){
-        return 1;
-       }
-       return a+zarb(a,b-1);
-}
-  int fact(int n){
-      if(n==1){
-        return 6;
-      }
-       return n*fact(n-1);
-  }
-  struct Calculatur{
-      int lv,rv;
-      char op;
-      void run()
-  {
-      cout<<"Enter oparator:";
-        cin>>op;
-        switch(op){
-            case'^':
-             cin>>lv>>rv;
-             cout<<lv<<rv;
-             break;
-             case'f':
-             cin>>lv;
-             cout<<fibo(lv);
-             break;
-             case'*':
-             cin>>lv;
-             cout<<fact(lv);
-             break;
-             case'!':
-             cin>>lv;
-             cout<<fact(lv);
-             break;
-             detalt:
-                 cout<<"Bad!";
-        }
-        }
-
-  };
-
-
 int main()
 {
-    Calculatur c;
-    c.run();
-    return 0;
+ float n1,n2,n3;
+ int w1,w2,w3;
+ cin>>n1>>n2>>n3;
+ cin>>w1>>w2>>w3;
+ float avg=(n1*w1+n2*w2+w3*w3)/(w1+w2+w3);
+ cout<<avg;
+  return 0;
 }
