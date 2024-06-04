28860af980328ea363b0602c399fde10d42eaa09
vidashakeri4911 <167344679+vidashakeri4911@users.noreply.github.com>
2024-06-04 22:13:33 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,18 +1,15 @@
-
 #include <iostream>
+
+using namespace std;
+
 int main()
 {
-   int i,j ;
-   for(i = 1 ; i<= 3 ; i++)
-   {
-       for(j=1;j<=i;j++)
-       {
-           printf("*");
-       }
-   printf("\n");
-   }
-
-   return 0;
+    int hour;
+    cin>>hour;
+    if(hour>12){
+        cout<<"PM";
+    }else{
+        cout<<"AM";
+    }
+    return 0;
 }
-
-
