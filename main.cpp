933dc6f00984cd2c13e11c5f5035d56d49a00482
vidashakeri4911 <167344679+vidashakeri4911@users.noreply.github.com>
2024-06-05 22:26:15 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,16 +1,55 @@
 #include <iostream>
 
 using namespace std;
+int inRange(int x,int low,int high){
+    if(x>low&&x<high){
+        return 0;
+    }
+    else if(x<low&&x>high){
+        return 0;
+    }
+    else if(x>low&&x>high){
+            return 1;
+    }else{
+        return -1;
+    }
+
+}
+int inRang(char x,char low,char high){
+    if(x>low&&x<high){
+        return 0;
+    }
+     else if(x<low&&x>high){
+
+
+        return 0;
+}
+
+else if(x>low&&x>high){
+            return 1;
+}
+else{
+        return -1;
+    }
+
+}
 
 int main()
 {
-    char a;
-    cin>>a;
-    if('9'>=a||'1'<=a){
-        cout<<"your char is number";
+    char gens;
+    cout<<"lmport int or char?(i or c)";
+    cin>>gens;
+    if(gens=='c'||gens=='C'){
+        char a,b,c;
+        cout<<"enter 3 char:";
+        cin>>a>>b>>c;
+        cout<<inRange(a,b,c);
     }
-    else{
-        cout<<"your char is'nt number";
+    if(gens=='i'||gens=='l'){
+        int a,b,c;
+        cout<<"enter 3 integer:";
+        cin>>a>>b>>c;
+        cout<<inRange(a,b,c);
     }
-    return 0;
+    return 1;
 }
