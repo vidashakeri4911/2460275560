c8b3d881e10577acfeb45ebdc00f7be160be7b4d
vidashakeri4911 <167344679+vidashakeri4911@users.noreply.github.com>
2024-06-05 17:36:14 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,18 +4,16 @@ using namespace std;
 
 int main()
 {
-    int n,tempNumber;
-    cin>>n;
-    cin>>tempNumber;
-    int max=tempNumber,min=tempNumber;
-    for(int i=1;i<n;i++){
-        cin>>tempNumber;
-        if(max<tempNumber){
-            max=tempNumber;
-            min=tempNumber;
-            }
+  int a;
+  cin>>a;
+  int modA=0;
+  for(int i=2;i<(a/2);i++){
+    if(a&i==0){
+        modA++;
     }
-    cout<<"Max:"<<max;
-    cout<<"Min:"<<min;
+    if(modA==0){
+        cout<<"It is primary";
+    }
+  }
     return 0;
 }
