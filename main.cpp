eb1f6919c3b9fd1ea72e385ffc61a3d556777aed
vidashakeri4911 <167344679+vidashakeri4911@users.noreply.github.com>
2024-06-05 12:56:37 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,42 +1,22 @@
 #include <iostream>
 
 using namespace std;
-void getArray(int a[100],int n){
-for(int i=0;i<n;i++){
-    cout<<"a["<<i<<"]:";
-    cin>>a[i];
-}
-}
-void printArray(int a[100],int n){
-    for(int j=0;j<n;j++){
-        cout<<"a["<<j<<"]:";
-        cout<<a[j]<<endl;
-    }
-}
 
-int findMax(int a[100],int n)
-{
-    int max=a[0],index=0;
-    for(int i=0;i<n;i++){
-        if(a[i]>max){
-            index=i;
-        }
-    }
-    return index;
-}
 int main()
 {
-    int n;
-    int x[100];
-    cout<<"Please enter n:";
-    cin>>n;
-    cout<<"EnterArray Data:"<<endl;
-    getArray(x,n);
-    cout<<"ArrayData:"<<endl;
-    printArray(x,n);
-    cout<<"MaxIndex:"<<findMax(x,n)<<endl;
-    cout<<"MaxValue:"<<x[findMax(x,n)];
-
-
-    return 2;
+    int number;
+    cout<<"Enter number:";
+    cin>>number;
+    if(number>=0&& number<-10){
+        if(number%2==0){
+            cout<<"Even"<<endl;
+            if(number<5){
+                cout<<"smallerthan 5";
+            }
+            else{
+                cout<<"Biggerthan 5";
+            }
+        }
+    }
+    return 0;
 }
