6cd6609acea7148041f407fdc1af1c592546d62a
vidashakeri4911 <167344679+vidashakeri4911@users.noreply.github.com>
2024-06-06 19:56:18 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,21 +1,56 @@
 #include <iostream>
 
 using namespace std;
-int f(int a){
-    if(a<10){
-        return a;
+int pow(int a,int b){
+    if(b==0){
+        return b;
     }
-    else{
-        cout<<a%10;
-        return f(a/10);
-    }
+    return a*pow(a,b-1);
 }
+
+int zarb(int a,int b){
+    if(b==0){
+        return 1;
+    }
+    return a+zarb(a,b-1);
+}
+struct calculator{
+    int lv,rv;
+    char op;
+    void run()
+    {
+        cout<<"enter operator"<<endl;
+        cin>>op;
+        switch(op){
+        case'^':
+        cin>>lv>>rv;
+        cout<<pow(lv,rv);
+        break;
+        case'*':
+        cin>>lv>>rv;
+        cout<<zarb(lv,rv);
+        break;
+        case'+':
+        cin>>lv>>rv;
+        cout<<(lv+rv);
+        break;
+        case'-':
+        cin>>lv>>rv;
+        cout<<(lv-rv);
+        break;
+        case'/':
+        cin>>lv>>rv;
+        cout<<(lv/rv);
+        break;
+        default:
+        cout<<"Bad!";
+        }
+    }
+};
+
 int main()
 {
-    int i;
-    cout<<"enter a number";
-    cin>>i;
-    cout<<f(i);
-
+    calculator c;
+    c.run();
     return 0;
 }
