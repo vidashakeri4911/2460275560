466356392028695751e861b5ed3ecf248b9eb0ae
vidashakeri4911 <167344679+vidashakeri4911@users.noreply.github.com>
2024-06-06 21:18:46 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,19 +1,40 @@
-#include <iostream>
-
+#include<iostream>
+#include<cmath>
 using namespace std;
-struct point{
-    double x;
-    double y;
+struct Point{
+    int x,y,z;
 };
-void calculateline(point p1,point p2){
-    double a=(p2.y-p1.y)/(p2.x-p1.x);
-    double b=p1.y-a*p1.x;
-    cout<<"y="<<a<<"x+"<<b<<endl;
+float calDistance(Point p1,Point p2){
+
+    return sqrt(pow(p2.x-p1.x,2)+pow(p2.y-p2.y,2)+pow(p2.z-p1.z,2));
+
+}
+
+Point getpoint(){
+Point myp;
+cout<<"enter  x y z for point:"<<endl;
+cin>>myp.x>>myp.y>>myp.z;
+
+return myp;
+}
+void z(Point p1,Point p2){
+    if(p1.z=p2.z){
+        cout<< " p1,p2 dar yek safhe hastan!"<<endl;
+        if(p1.x=p2.x){
+            cout<<"har do dar safhe y";
+        }else if(p1.y=p2.y){
+            cout<<"har do dar safhe x";
+        }
+    }else{
+        cout<<" p1,p2 dar yek safhe nstan!";
+    }
 }
 int main()
 {
-    point p1={1.0,2.0};
-    point p2={3.0,4.0};
-    calculateline(p1,p2);
+    Point p1=getpoint();
+    Point p2=getpoint();
+    float d=calDistance(p1,p2);
+    cout<<"D:"<<d<<endl;
+    z(p1,p2);
     return 0;
 }
