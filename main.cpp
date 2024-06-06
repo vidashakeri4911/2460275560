433567d42da2bf3791357baa92458c4b6ddfca16
vidashakeri4911 <167344679+vidashakeri4911@users.noreply.github.com>
2024-06-06 18:10:44 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,32 +1,21 @@
-//calculator with switch.
 #include <iostream>
-using namespace std;
-int Operation (int a ,int b, char op)
-{
-    switch (op)
-        {
-        case '+':
-            cout << a+b;
-            break;
-        case '-':
-            cout <<a-b;
-            break;
-        case '*':
-            cout << a*b;
-            break;
-        case '/':
-            cout <<a/b;
-            break;
 
-        }
-    return 0;
+using namespace std;
+int f(int a){
+    if(a<10){
+        return a;
+    }
+    else{
+        cout<<a%10;
+        return f(a/10);
+    }
 }
 int main()
 {
-    int a,b;
-    char op;
-    cin >>a>>op>>b;
-    Operation(a,b,op);
-    cout << endl;
+    int i;
+    cout<<"enter a number";
+    cin>>i;
+    cout<<f(i);
+
     return 0;
 }
