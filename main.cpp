f0dfafd6147574c432e7c61f1c3a86665ceb43bf
vidashakeri4911 <167344679+vidashakeri4911@users.noreply.github.com>
2024-06-06 16:30:20 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,63 +1,20 @@
 #include <iostream>
-
+#define pi 3.14
 using namespace std;
-int pow(int a,int b){
-    if(b==0){
-        return b;
-    }
-    return a*pow(a,b-1);
+float masahat(int n)
+{
+    return pi*n*n;
 }
-int fibo(int n){
-    if(n==1||n==2){
-        return 1;
-    }
-    return fibo(n-2)+fibo(n-1);
+float mohit(int a)
+{
+    return pi*a*2;
 }
-int zarb(int a,int b){
-    if(b==0){
-        return 1;
-    }
-    return a+zarb(a,b-1);
-}
-int fact(int n){
-    if(n==1){
-        return 1;
-    }
-    return n*fact(n-1);
-}
-struct calculator{
-    int lv,rv;
-    char op;
-    void run()
-    {
-        cout<<"enter operator"<<endl;
-        cin>>op;
-        switch(op){
-        case'^':
-            cin>>lv>>rv;
-            cout<<pow(lv,rv);
-            break;
-            case'f':
-            cin>>lv;
-            cout<<fibo(lv);
-            break;
-            case'*':
-            cin>>lv>>rv;
-            cout<<zarb(lv,rv);
-            break;
-            case'!':
-            cin>>lv;
-            cout<<fact(lv);
-            break;
-            default:
-            cout<<"Bad!";
-        }
-    }
-};
-
 int main()
 {
-    calculator c;
-    c.run();
+    int r;
+    cout<<"enter Radius:";
+    cin>>r;
+    cout<<"Area of circle is:"<<masahat(r)<<endl;
+    cout<<"perimeter of circle is:"<<mohit(r);
     return 0;
 }
