2f0fb782aac97a9aa0dcb4e4d6471a4677b85e09
vidashakeri4911 <167344679+vidashakeri4911@users.noreply.github.com>
2024-06-06 16:22:21 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,33 +4,16 @@ using namespace std;
 
 int main()
 {
-    int a1,a2,a3,n=0;
-    int coutPositire=0,coutNegetive=0;
-    float avrage;
-    cin>>a1>>a2>>a3;
-    if(a1>0){
-        coutPositire++;
+    int a,x;
+    cout<<"enter a number"<<endl;
+    cin>>a;
+    for(int i=1;i<=100;i++){
+        x=a*i;
+        if(x<=100){
+            cout<<i<<"x"<<a<<"="<<x<<endl;
+        }else{
+        break;
+        }
     }
-    if(a2>0){
-        coutPositire++;
-    }
-    if(a3>0){
-        coutPositire++;
-    }
-
-    if(a1<0){
-        coutNegetive++;
-    }
-    if(a2<0){
-        coutNegetive++;
-    }
-    if(a3<0){
-       coutNegetive++;
-    }
-    avrage=(a1+a2+a3)/3.0;
-    cout<<"Avrage:"<<avrage<<endl;
-    cout<<"N:"<<coutNegetive<<endl;
-    cout<<"P:"<<coutPositire;
-
     return 0;
 }
