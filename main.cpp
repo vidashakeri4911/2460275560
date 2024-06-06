cbb8b2fe3d1632ba8c9887fbb5890d6c17ac856e
vidashakeri4911 <167344679+vidashakeri4911@users.noreply.github.com>
2024-06-06 16:27:19 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,15 +1,63 @@
 #include <iostream>
 
 using namespace std;
-
+int pow(int a,int b){
+    if(b==0){
+        return b;
+    }
+    return a*pow(a,b-1);
+}
+int fibo(int n){
+    if(n==1||n==2){
+        return 1;
+    }
+    return fibo(n-2)+fibo(n-1);
+}
+int zarb(int a,int b){
+    if(b==0){
+        return 1;
+    }
+    return a+zarb(a,b-1);
+}
+int fact(int n){
+    if(n==1){
+        return 1;
+    }
+    return n*fact(n-1);
+}
+struct calculator{
+    int lv,rv;
+    char op;
+    void run()
+    {
+        cout<<"enter operator"<<endl;
+        cin>>op;
+        switch(op){
+        case'^':
+            cin>>lv>>rv;
+            cout<<pow(lv,rv);
+            break;
+            case'f':
+            cin>>lv;
+            cout<<fibo(lv);
+            break;
+            case'*':
+            cin>>lv>>rv;
+            cout<<zarb(lv,rv);
+            break;
+            case'!':
+            cin>>lv;
+            cout<<fact(lv);
+            break;
+            default:
+            cout<<"Bad!";
+        }
+    }
+};
 
 int main()
 {
-    int x;
-    cout<<"Enter number:";
-    cin>>x;
-    for(int i=0;i<=100;i++){
-        cout<<'i'<<'*'<<'x'<<'='<<(x*i)<<'\n';
-    }
+    calculator c;
+    c.run();
     return 0;
 }
