a52ff7b1343bd2b2f54e59392fbe59319844670d
vidashakeri4911 <167344679+vidashakeri4911@users.noreply.github.com>
2024-06-06 16:24:12 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,18 +2,14 @@
 
 using namespace std;
 
+
 int main()
 {
-    int a,x;
-    cout<<"enter a number"<<endl;
-    cin>>a;
-    for(int i=1;i<=100;i++){
-        x=a*i;
-        if(x<=100){
-            cout<<i<<"x"<<a<<"="<<x<<endl;
-        }else{
-        break;
-        }
+    int x;
+    cout<<"Enter number:";
+    cin>>x;
+    for(int i=0;i<=100;i++){
+        cout<<'i'<<'*'<<'x'<<'='<<(x*i)<<'\n';
     }
     return 0;
 }
